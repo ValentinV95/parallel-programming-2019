bb8562a9183c1436d6687778a2f7c81afcb88459
AVBalynin <alexbalynin@gmail.com>
2019-10-19 20:01:52 +0300

    fix

--- 1708-2/balynin_av/mpi_1.cpp
+++ 1708-2/balynin_av/mpi_1.cpp
@@ -6,6 +6,7 @@
 #include <mpi.h>
 #include <vector>
 #include <time.h>
+#include <stdexcept>
 #include <algorithm>
 using namespace std;
 
@@ -20,53 +21,49 @@ vector<int> GenerateVector(int Size, int MaxVal) {
 	cout << "\nVector: ";
 	for (int i = 0; i < Size; i++)
 		Myvector[i] = (rand() % MaxVal + 1);
-	for (int n : Myvector)
-		cout << n << "\t";
 	return Myvector;
 }
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
+	int ProcNum, Rank, Size = 1000000, MaxVal = 1024;
 	double TimeStartWork, TimeEndWork;
-	int Size, MaxVal, *Data, Rank, ProcNum;
-	vector<int> Myvector;
-	cout << "Search Max Element Vector\nSize Vector: ";
-	cin >> Size;
-	cout << "Max Value At Elements Vector: ";
-	cin >> MaxVal;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-	int PartSize = Size / ProcNum;
-	int *Part = new int[PartSize];
-	Data = new int[Size];
 	MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+	vector<int> Myvector(Size);
+	Myvector = GenerateVector(Size, MaxVal);
+	int PartSize = Size / ProcNum;
+	int Residue = Size % ProcNum;
+	vector<int> Part(PartSize);
 	if (Rank == 0) {
-		int Max;
-		Myvector = GenerateVector(Size, MaxVal);
 		TimeStartWork = MPI_Wtime();
-		Max = MaxElem(Myvector);
+		int Max = MaxElem(Myvector);
 		TimeEndWork = MPI_Wtime();
-		cout << endl << "Max In Single = " << Max << "\nTime In Single " << TimeStartWork - TimeEndWork << " sec" << endl;
-		MPI_Scatter(Data, PartSize, MPI_INT, Part, PartSize, MPI_INT, 0, MPI_COMM_WORLD);
+		cout << endl << "Max In Single = " << Max << "\nTime In Single = " << TimeStartWork - TimeEndWork << " sec" << endl;
+		for (int i = 1; i < ProcNum; i++)
+			MPI_Send(&Myvector[Residue] + i * PartSize, PartSize, MPI_INT, i, 0, MPI_COMM_WORLD);
 	}
-	else
-		MPI_Scatter(nullptr, PartSize, MPI_INT, Part, PartSize, MPI_INT, 0, MPI_COMM_WORLD);
-	TimeStartWork = MPI_Wtime();
-	int Max = MaxElem(Myvector);
+	else if (Rank > 0) {
+		TimeStartWork = MPI_Wtime();
+		MPI_Status Status;
+		vector<int> SubVec(PartSize);
+		MPI_Recv(&SubVec[0], PartSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+		int Max = *max_element(SubVec.begin(), SubVec.end());
+		MPI_Send(&Max, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+	}
 	if (Rank == 0) {
-		int result;
-		MPI_Reduce(&Max, &result, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-		if (Size % ProcNum != 0) {
-			for (int i = PartSize * ProcNum; i < Size; i++) {
-				if (result > Data[i])
-					result = Data[i];
-			}
+		int ResMax;
+		MPI_Status  Status;
+		int Result = *max_element(Myvector.begin(), Myvector.begin() + Residue);
+		for (int i = 1; i < Size; i++) {
+			MPI_Recv(&ResMax, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &Status);
+			Result = max(Result, ResMax);
+			TimeEndWork = MPI_Wtime();
+			cout << "Max In Parallel =  " << Result << "\nTime In Parallel = " << std::fixed << TimeStartWork - TimeEndWork << " sec" << endl;
 		}
-		TimeEndWork = MPI_Wtime();
-		cout << "Max In Parallel =  " << result << endl << "Time In Parallel = " << std::fixed << TimeStartWork - TimeEndWork << " sec" << endl;
 	}
-	else
-		MPI_Reduce(&Max, nullptr, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Finalize();
 	return 0;
 }
