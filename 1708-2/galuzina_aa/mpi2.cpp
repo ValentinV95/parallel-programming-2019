880789ce4f9ff0c81108a79f6afb2b6a3afa8b77
A.Galuzina <42965044+Galuzina@users.noreply.github.com>
2020-02-19 15:19:54 +0300

    Update mpi2.cpp

--- 1708-2/galuzina_aa/mpi2.cpp
+++ 1708-2/galuzina_aa/mpi2.cpp
@@ -27,9 +27,9 @@ int getPositive_elem(std::vector<int> vector, int count_size_vector) {
 	}
 	return vector.size() - negative_elem;
 }
-int* Create_dinamic_massiv_from_vector(std::vector<int> vec) {//принимает вектор, возвращает массив
-	int vec_size = vec.size(); //записыавем размер вектора
-	int* mas = new int[vec_size];//создаем массив размера это вектора
+int* Create_dinamic_massiv_from_vector(std::vector<int> vec) {//РїСЂРёРЅРёРјР°РµС‚ РІРµРєС‚РѕСЂ, РІРѕР·РІСЂР°С‰Р°РµС‚ РјР°СЃСЃРёРІ
+	int vec_size = vec.size(); //Р·Р°РїРёСЃС‹Р°РІРµРј СЂР°Р·РјРµСЂ РІРµРєС‚РѕСЂР°
+	int* mas = new int[vec_size];//СЃРѕР·РґР°РµРј РјР°СЃСЃРёРІ СЂР°Р·РјРµСЂР° СЌС‚Рѕ РІРµРєС‚РѕСЂР°
 	for (int i = 0; i < vec_size; i++) {
 		mas[i] = vec[i];
 	}
@@ -40,7 +40,7 @@ int Consumer(int *buffer, int buffer_size, int rank_proc, int* resurce) {
 	int size, rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (size == 1 || rank_proc == 0) { // если 0 процес выполнет функцию или кол-во проц = 1
+	if (size == 1 || rank_proc == 0) { // РµСЃР»Рё 0 РїСЂРѕС†РµСЃ РІС‹РїРѕР»РЅРµС‚ С„СѓРЅРєС†РёСЋ РёР»Рё РєРѕР»-РІРѕ РїСЂРѕС† = 1
 		for (int i = 0; i < buffer_size; i++) {
 			if (buffer[i] != -1) {
 				*resurce = buffer[i];
@@ -59,13 +59,13 @@ int Consumer(int *buffer, int buffer_size, int rank_proc, int* resurce) {
 					break;
 				}
 			}
-			MPI_Send(&temp_resurs, 1, MPI_INT, rank_proc, 0, MPI_COMM_WORLD); //отправляет ресурс процессору который его вызвал
+			MPI_Send(&temp_resurs, 1, MPI_INT, rank_proc, 0, MPI_COMM_WORLD); //РѕС‚РїСЂР°РІР»СЏРµС‚ СЂРµСЃСѓСЂСЃ РїСЂРѕС†РµСЃСЃРѕСЂСѓ РєРѕС‚РѕСЂС‹Р№ РµРіРѕ РІС‹Р·РІР°Р»
 		}
 		else {
 			if (rank == rank_proc) {
 				int temp_resurs;
-				MPI_Recv(&temp_resurs, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);//принимает данные от производителя на 0
-				*resurce = temp_resurs; // отправленный ресурс делает принятым 
+				MPI_Recv(&temp_resurs, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);//РїСЂРёРЅРёРјР°РµС‚ РґР°РЅРЅС‹Рµ РѕС‚ РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЏ РЅР° 0
+				*resurce = temp_resurs; // РѕС‚РїСЂР°РІР»РµРЅРЅС‹Р№ СЂРµСЃСѓСЂСЃ РґРµР»Р°РµС‚ РїСЂРёРЅСЏС‚С‹Рј 
 			}
 		}
 	}
@@ -79,29 +79,29 @@ int Producer(int *buffer, int buffer_size, int rank_proc, int resurce) {
 	if (size == 1 || rank_proc == 0) {
 		for (int i = 0; i < buffer_size; i++) {
 			if (buffer[i] == -1) {
-				buffer[i] = resurce; //заполнение буфера ресурсом
+				buffer[i] = resurce; //Р·Р°РїРѕР»РЅРµРЅРёРµ Р±СѓС„РµСЂР° СЂРµСЃСѓСЂСЃРѕРј
 				break;
 			}
 		}
 	}
 	else {
-		if (rank == 0) { //если функцию вызвал 0 процессор, то принимает
+		if (rank == 0) { //РµСЃР»Рё С„СѓРЅРєС†РёСЋ РІС‹Р·РІР°Р» 0 РїСЂРѕС†РµСЃСЃРѕСЂ, С‚Рѕ РїСЂРёРЅРёРјР°РµС‚
 			int resurce_for_bufer;
-			MPI_Recv(&resurce_for_bufer, 1, MPI_INT, rank_proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //прием сообщения процессора вызывающего функцию производителей
-			for (int i = 0; i < buffer_size; i++) { //по кол-ву эл буфера заполнение буфера ресурсом
+			MPI_Recv(&resurce_for_bufer, 1, MPI_INT, rank_proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //РїСЂРёРµРј СЃРѕРѕР±С‰РµРЅРёСЏ РїСЂРѕС†РµСЃСЃРѕСЂР° РІС‹Р·С‹РІР°СЋС‰РµРіРѕ С„СѓРЅРєС†РёСЋ РїСЂРѕРёР·РІРѕРґРёС‚РµР»РµР№
+			for (int i = 0; i < buffer_size; i++) { //РїРѕ РєРѕР»-РІСѓ СЌР» Р±СѓС„РµСЂР° Р·Р°РїРѕР»РЅРµРЅРёРµ Р±СѓС„РµСЂР° СЂРµСЃСѓСЂСЃРѕРј
 				if (buffer[i] == -1) {
 					buffer[i] = resurce;
 					break;
 				}
 			}
 		}
-		else { //если любой другой, то отправка нулевому 
+		else { //РµСЃР»Рё Р»СЋР±РѕР№ РґСЂСѓРіРѕР№, С‚Рѕ РѕС‚РїСЂР°РІРєР° РЅСѓР»РµРІРѕРјСѓ 
 			if (rank == rank_proc) {
 				MPI_Send(&resurce, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 			}
 		}
 	}
-	MPI_Barrier(MPI_COMM_WORLD); //Блокирует звонящего до тех пор, пока все члены группы не вызовут его; вызов возвращается в любом процессе только после того, как все члены группы вошли в вызов.
+	MPI_Barrier(MPI_COMM_WORLD); //Р‘Р»РѕРєРёСЂСѓРµС‚ Р·РІРѕРЅСЏС‰РµРіРѕ РґРѕ С‚РµС… РїРѕСЂ, РїРѕРєР° РІСЃРµ С‡Р»РµРЅС‹ РіСЂСѓРїРїС‹ РЅРµ РІС‹Р·РѕРІСѓС‚ РµРіРѕ; РІС‹Р·РѕРІ РІРѕР·РІСЂР°С‰Р°РµС‚СЃСЏ РІ Р»СЋР±РѕРј РїСЂРѕС†РµСЃСЃРµ С‚РѕР»СЊРєРѕ РїРѕСЃР»Рµ С‚РѕРіРѕ, РєР°Рє РІСЃРµ С‡Р»РµРЅС‹ РіСЂСѓРїРїС‹ РІРѕС€Р»Рё РІ РІС‹Р·РѕРІ.
 	return 0;
 }
 int main(int argc, char** argv)
@@ -109,41 +109,41 @@ int main(int argc, char** argv)
 	setlocale(LC_ALL, "Russian");
 	int rank, size;
 	double st1, st2, et1, et2;  //time
-	MPI_Init(&argc, &argv); //принимает данные с консоли иницализирует работы 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // возвращает ранг процессоров 
-	MPI_Comm_size(MPI_COMM_WORLD, &size); //количество
+	MPI_Init(&argc, &argv); //РїСЂРёРЅРёРјР°РµС‚ РґР°РЅРЅС‹Рµ СЃ РєРѕРЅСЃРѕР»Рё РёРЅРёС†Р°Р»РёР·РёСЂСѓРµС‚ СЂР°Р±РѕС‚С‹ 
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // РІРѕР·РІСЂР°С‰Р°РµС‚ СЂР°РЅРі РїСЂРѕС†РµСЃСЃРѕСЂРѕРІ 
+	MPI_Comm_size(MPI_COMM_WORLD, &size); //РєРѕР»РёС‡РµСЃС‚РІРѕ
 	int kol_elem_in_buffer = 100;
 	int *buffer = new int[kol_elem_in_buffer];
 	for (int i = 0; i < kol_elem_in_buffer; i++) {
 		buffer[i] = -1;
 	}
-	int kol_resursov = atoi(argv[1]); //задаем кол-во ресурсов с консоли
+	int kol_resursov = atoi(argv[1]); //Р·Р°РґР°РµРј РєРѕР»-РІРѕ СЂРµСЃСѓСЂСЃРѕРІ СЃ РєРѕРЅСЃРѕР»Рё
 	if (size == 1) {
 		st2 = MPI_Wtime();
 		for (int i = 0; i < kol_resursov; i++) {
 			Producer(buffer, kol_elem_in_buffer, rank, i);
 		}
-		std::vector<int> resurce_consume1(kol_resursov, -1); //заполнение -1
+		std::vector<int> resurce_consume1(kol_resursov, -1); //Р·Р°РїРѕР»РЅРµРЅРёРµ -1
 		int *resurce_consume;
-		resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1); //вернула массив
+		resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1); //РІРµСЂРЅСѓР»Р° РјР°СЃСЃРёРІ
 		for (int i = 0; i < kol_resursov; i++) {
-			Consumer(buffer, kol_elem_in_buffer, rank, &resurce_consume[i]); //отправляю данные в массиве 
+			Consumer(buffer, kol_elem_in_buffer, rank, &resurce_consume[i]); //РѕС‚РїСЂР°РІР»СЏСЋ РґР°РЅРЅС‹Рµ РІ РјР°СЃСЃРёРІРµ 
 			et2 = MPI_Wtime();
 			cout << resurce_consume[i] << "\n" << "Time_Single" << st2-et2 << std::fixed << " sec \n";;
 		}
 	}
-	else { // многопроцесорная 
+	else { // РјРЅРѕРіРѕРїСЂРѕС†РµСЃРѕСЂРЅР°СЏ 
 		st1 = MPI_Wtime();
 		for (int i = 0; i < kol_resursov; i++) {
-			Producer(buffer, kol_elem_in_buffer, 1, i); //отправляем по кол-ву ресурсов отправляем кол-во эл в буфере 
+			Producer(buffer, kol_elem_in_buffer, 1, i); //РѕС‚РїСЂР°РІР»СЏРµРј РїРѕ РєРѕР»-РІСѓ СЂРµСЃСѓСЂСЃРѕРІ РѕС‚РїСЂР°РІР»СЏРµРј РєРѕР»-РІРѕ СЌР» РІ Р±СѓС„РµСЂРµ 
 		}
 		std::vector<int> resurce_consume1(kol_resursov, -1);
 		int *resurce_consume;
 		resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
 		for (int i = 0; i < kol_resursov; i++) {
-			Consumer(buffer, kol_elem_in_buffer, 1, &resurce_consume[i]); //отправляю -1 (массив)
+			Consumer(buffer, kol_elem_in_buffer, 1, &resurce_consume[i]); //РѕС‚РїСЂР°РІР»СЏСЋ -1 (РјР°СЃСЃРёРІ)
 		}
-		if (rank == 1) { //выписваю ресурсы если ранг 1
+		if (rank == 1) { //РІС‹РїРёСЃРІР°СЋ СЂРµСЃСѓСЂСЃС‹ РµСЃР»Рё СЂР°РЅРі 1
 			for (int i = 0; i < kol_resursov; i++) {
 				et1 = MPI_Wtime();
 				cout << resurce_consume[i] << "\n" << "Time_Multyply" << st1 - et1 << std::fixed << " sec \n";;
