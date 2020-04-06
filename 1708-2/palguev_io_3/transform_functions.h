bfe9249f698d902861aa7280f4b9c3267255e47d
ArtemSadikov <marolboron@ya.ru>
2020-04-06 21:33:26 +0300

    done 3

---
+++ 1708-2/palguev_io_3/transform_functions.h
@@ -0,0 +1,6 @@
+#ifndef TRANSFORM_FUNCTIONS_H
+#define TRANSFORM_FUNCTIONS_H
+double TransformFunction1(double input);
+double TransformFunction2(double input);
+double TransformFunction3(double input);
+#endif // TRANSFORM_FUNCTIONS_H
