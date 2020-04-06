442e6d4d948dbdd88b869c00e3e49273c6c406c1
ArtemSadikov <marolboron@ya.ru>
2020-04-06 23:21:23 +0300

    ready

---
+++ 1708-2/palguev_io_3/transform_functions.cpp
@@ -0,0 +1,18 @@
+// Copyright 2019 Palguev Igor
+#include <cmath>
+#include "../../../modules/task_3/palguev_i_strongin/transform_functions.h"
+
+double TransformFunction1(double input)
+{
+	return 100 + std::cos(input * 10);
+}
+
+double TransformFunction2(double input)
+{
+	return std::sqrt(input) * std::sin(input);
+}
+
+double TransformFunction3(double input)
+{
+	return std::sqrt(1 + 3 * std::pow(std::cos(input), 2)) + std::cos(10 * input);
+}
---
+++ 1708-2/palguev_io_3/transform_functions.h
@@ -0,0 +1,10 @@
+// Copyright 2019 Palguev Igor
+
+#ifndef MODULES_TASK_3_PALGUEV_I_STRONGIN_TRANSFORM_FUNCTIONS_H_
+#define MODULES_TASK_3_PALGUEV_I_STRONGIN_TRANSFORM_FUNCTIONS_H_
+
+double TransformFunction1(double input);
+double TransformFunction2(double input);
+double TransformFunction3(double input);
+
+#endif  // MODULES_TASK_3_PALGUEV_I_STRONGIN_TRANSFORM_FUNCTIONS_H_
