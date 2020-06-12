b3f7d06aff349764aa632244549dbf81ffd8549d
utshomomen <mohibulmomenutsho@yahoo.com>
2020-06-12 13:26:03 +0300

    secondtry5thlab

--- 1708-2/openmpshell.cpp
+++ 1708-2/openmpshell.cpp
@@ -1,8 +1,4 @@
-﻿//g++ -c openmpshell.cpp -o openmpshell.o -fopenmp
-//g++ openmpshell.cpp openmpshell -fopenmp -lpthread
-
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include "malloc.h"
 #include "omp.h"
@@ -32,13 +28,34 @@ void ShellSort(int a[], int n)
 #pragma omp critical
 				if (element < a[j - gap])
 					a[j] = a[j - gap];
-				else
-					break;
+
 			}
-			a[j] = element;
+
+					a[j] = element;
+			
 		}
 	}
 }
+
+void mergeSort(int a[], int t[], int n) 
+{
+#pragma omp parallel sections
+	if (n > 1) {
+#pragma omp section
+		for (int i = 0; i < n / 2; i++) {
+			t[i] = a[i];
+		}
+#pragma omp section
+		mergeSort(a, t, n / 2);
+#pragma omp section
+		for (int i = n / 2; i < n; i++) {
+			t[i] = a[i];
+		}
+		mergeSort(a, t, n / 2);
+		ShellSort(a, n);
+	}
+}
+
 
 
 void print(int a[], int size)
@@ -53,10 +70,9 @@ void print(int a[], int size)
 
 int main()
 {
-
 	int n = 2000;
 	int a[2000];
-	int i ;
+	int i;
 	srand((time(NULL)));
 
 #pragma omp parallel for num_threads(i)
@@ -74,28 +90,44 @@ int main()
 	for (int i = 0; i < n; i++)
 	{
 		cin >> a[i];
+		
 	}
 	cout << "array seq before sorting: ";
+
 	print(a, n);
 	ShellSort(a, n);
+
+	
+
 	cout << "array seq after sorting: ";
 	print(a, n);
 
 
+	cout << "enter the size of the array" << endl;
+	cin >> n;
+	int arr[2000];
+	cout << "please enter the elements of the array" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "enter the element of merge" << i << endl;
+		cin >> arr[i];
+	}
+
+	mergeSort(arr, 0, n - 1);
+
+	cout << "\tSorted Array Elements with shell" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << "\t";
+	}
 
 	ShellSort(a, n);
 	double endTime = clock();
 	int totalTime = endTime - startTime; // The average time to run this
 
-	cout << "This is the time it took to run.\n" << endl; 
-	cout<< totalTime / 2000 <<n<< endl;
+	cout << "This is the time it took to run.\n" << endl;
+	cout << totalTime / 2000 << n << endl;
 
 
 
 
 
 }
-
-
-
-
