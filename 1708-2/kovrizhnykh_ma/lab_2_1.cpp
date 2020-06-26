dcc228ec369213f746ae815936c8d84e65ae522f
Мария Коврижных <43151557+mkovrizhnykh@users.noreply.github.com>
2020-06-26 19:34:13 +0300

    update lab 2.1 (again and again)

--- 1708-2/kovrizhnykh_ma/lab_2_1.cpp
+++ 1708-2/kovrizhnykh_ma/lab_2_1.cpp
@@ -2,46 +2,47 @@
 #include <time.h>
 #include <stdio.h>
 #include <limits.h>
+#include <iostream>
 
-void quickSort(double* array, int left, int right)
-{
-    while (right > left)
-	{
-        int iterate_left = left;
-        int iterate_right = right;
-        double pivot = array[(left + right) >> 1];
+void qsrt(double* arr, int low, int high) {
+	int i;
+	double pivot, t;
+    if (low < high) {
+        i = (low - 1);
+    	pivot = arr[high];
 
-        while (iterate_left <= iterate_right)
-		{
-            while (array[iterate_left] < pivot)
-			{
-                iterate_left += 1;
-            }
-            while (array[iterate_right] > pivot)
-			{
-                iterate_right -= 1;
-            }
-            if (iterate_left <= iterate_right)
-			{
-                double temp = array[iterate_left];
-                array[iterate_left] = array[iterate_right];
-                array[iterate_right] = temp;
+    	for (int j = low; j <= high - 1; j++)
+    	{
+        	if (arr[j] <= pivot)
+        	{
+            	i++;
+            	t = arr[i];
+    			arr[i] = arr[j];
+    			arr[j] = t;
+        	}
+    	}
+    	t = arr[i + 1];
+    	arr[i + 1] = arr[high];
+    	arr[high] = t;
+        qsrt(arr, low, i);
 
-                iterate_left += 1;
-                iterate_right -= 1;
-            }
-        }
+		i = (low - 1);
+    	pivot = arr[high];
 
-        if ((iterate_left << 1) > left + right)
-		{
-            quickSort(array, iterate_left, right);
-            right = iterate_left - 1;
-        }
-		else
-		{
-            quickSort(array, left, iterate_left - 1);
-            left = iterate_left;
-        }
+    	for (int j = low; j <= high - 1; j++)
+    	{
+        	if (arr[j] <= pivot)
+        	{
+            	i++;
+            	t = arr[i];
+    			arr[i] = arr[j];
+    			arr[j] = t;
+        	}
+    	}
+    	t = arr[i + 1];
+    	arr[i + 1] = arr[high];
+    	arr[high] = t;
+        qsrt(arr, i + 1, high);
     }
 }
 
@@ -75,14 +76,14 @@ bool isSorted(double* ar, int size) {
 int main(void) {
 	srand(time(NULL));
 
-	int size = 20000;
+	int size = 200000;
 	int threads = 4;
 
     double* seq = new double[size];
     getRandomArray(seq, size);
 
     clock_t start = clock();
-    quickSort(seq, 0, size - 1);
+    qsrt(seq, 0, size - 1);
     clock_t end = clock();
 	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
     printf("(Sequential) time for quicksort = %f \n", seconds);
@@ -93,7 +94,14 @@ int main(void) {
 		printf("Sorted correctly\n");
 	}
 
+	//print everything
+	/*
+	for (int i = 0; i < size; i++)
+        std::cout << seq[i] << " ";
+    std::cout << std::endl;*/
+
     delete[]seq;
 
 	return 0;
 }
+
