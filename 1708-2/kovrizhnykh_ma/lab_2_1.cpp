b2218ed2a460ebc56671440e31552ae7ee2e40a1
Мария Коврижных <43151557+mkovrizhnykh@users.noreply.github.com>
2020-06-28 16:05:35 +0300

    update lab2.1 (again x4)

--- 1708-2/kovrizhnykh_ma/lab_2_1.cpp
+++ 1708-2/kovrizhnykh_ma/lab_2_1.cpp
@@ -1,98 +1,119 @@
 #include <cstdlib>
-#include <time.h>
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
 
-template<typename T>
-void swp(T& a, T& b)
-{ T temp = a;
-  a = b;
-  b = temp;
-}
+using namespace std;
 
-int partition(double* arr, int low, int high)
-{
-    double pivot = arr[high];
-    int i = (low - 1);
+#define X 200
 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swp(arr[i], arr[j]);
-        }
-    }
-    swp(arr[i + 1], arr[high]);
-    return (i + 1);
-}
-
-void qsrt(double* arr, int low, int high) {
-	if (low < high)
-    {
-        int pi = partition(arr, low, high);
-
-        qsrt(arr, low, pi - 1);
-        qsrt(arr, pi + 1, high);
-    }
-}
-
-void getRandomArray(double* arr, int size)
-{
-	int i = 0;
-	double number;
-
-	while(i < size)
-	{
-		number = rand() / (RAND_MAX + 1.0);
-		arr[i] = number;
-		i += 1;
-	}
-}
-
-bool isSorted(double* ar, int size) {
-    const double *previous_value = ar;
-
-    while (size) {
-       if (*ar < *previous_value)
-             return false;
-       previous_value = ar;
-
-       ++ar;
-       --size;
-     }
-     return true;
-}
+void quicksort(int *, int, int);
+int partition(int *, int, int);
+int choosePivot(int *, int, int);
+void swap(int *, int *);
 
 int main(void) {
 	srand(time(NULL));
 
-	int size = 200000;
+	int size = 20000;
 	int threads = 4;
 
-    double* seq = new double[size];
-    getRandomArray(seq, size);
+	double* seq = new double[size];
+	
+	srand(123456);
 
-    clock_t start = clock();
-    qsrt(seq, 0, size - 1);
-    clock_t end = clock();
+	int * arr = (int *)malloc(sizeof(int)*size);
+	int i;
+	for (i = 0; i < size; i++)
+		arr[i] = rand() % X;
+
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+
+	cout << endl << "--------------" << endl;
+
+	clock_t start = clock();
+	quicksort(arr, 0, size - 1);
+	clock_t end = clock();
 	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-    printf("(Sequential) time for quicksort = %f \n", seconds);
 
-    if (!isSorted(seq, size)) {
-    	printf("Sorted incorrectly\n");
-    } else {
-		printf("Sorted correctly\n");
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+
+	cout << endl;
+	cout << "Time of sort = " << seconds << endl;
+
+	free((void *)arr);
+
+	system("pause");
+	
+	return 0;	
+	
+}
+
+void quicksort(int * arr, int lo, int hi)
+{
+	if (lo < hi)
+	{
+		int p = partition(arr, lo, hi);
+		quicksort(arr, lo, p - 1);
+		quicksort(arr, p + 1, hi);
+	}
+}
+
+int partition(int * arr, int lo, int hi)
+{
+	int i;
+	int pivotIdx = choosePivot(arr, lo, hi);
+	int pivotVal = arr[pivotIdx];
+
+	swap(&arr[pivotIdx], &arr[hi]);
+
+	int storeIdx = lo;
+
+	for (i = lo; i < hi; i++)
+	{
+		if (arr[i] < pivotVal)
+		{
+			swap(&arr[i], &arr[storeIdx]);
+			storeIdx++;
+		}
 	}
 
-	//print everything
-/*
-	for (int i = 0; i < size; i++)
-        std::cout << seq[i] << " ";
-    std::cout << std::endl;*/
+	swap(&arr[storeIdx], &arr[hi]);
+	return storeIdx;
+}
 
-    delete[]seq;
+void swap(int * x, int * y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
 
-	return 0;
+int choosePivot(int * arr, int lo, int hi)
+{
+	int mid = (lo + hi) / 2;
+
+	int temp;
+	if (arr[lo] > arr[hi])
+	{
+		temp = lo;
+		lo = hi;
+		hi = temp;
+	}
+	if (arr[mid] < arr[lo])
+	{
+		temp = mid;
+		mid = lo;
+		lo = temp;
+	}
+	if (arr[hi] < arr[mid])
+	{
+		temp = mid;
+		mid = hi;
+		hi = temp;
+	}
+	return mid;
 }
