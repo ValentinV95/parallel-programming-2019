03f3fdab51b932f7a51eac7f0e870faee2e9aff2
utshomomen <mohibulmomenutsho@yahoo.com>
2020-06-28 23:28:37 +0300

    tbb3try

--- 1708-2/tbbone.cpp
+++ 1708-2/tbbone.cpp
@@ -10,12 +10,11 @@
 #include <time.h>
 #include <cstdlib>
 #include <stdlib.h>
-
 #include "tbb/tbb.h"
 #include <thread>
-#include <mutex>
-#include "tbb/parallel_sort.h"
-
+#include <ctime>
+#include <algorithm>
+#include <utility>
 using namespace tbb;
 
 #define MAX_SIZE 1000
@@ -23,223 +22,112 @@ using namespace tbb;
 
 //a[] is sort , n is size
 
-void merge(int a[], int n, int t[]) {
-	int i = 0;
-	int j = n / 2;
-	int ti = 0;
-
-	while (i < n / 2 && j < n) {
-		if (a[i] < a[j]) {
-			t[ti] = a[i];
-			ti++; i++;
-		}
-		else {
-			t[ti] = a[j];
-			ti++; j++;
-		}
-	}
-	while (i < n / 2)
-	{
-		t[ti] = a[i];
-		ti++; i++;
-	}
-	while (j < n)
-	{
-
-		t[ti] = a[j];
-		ti++; j++;
-	}
-	memcpy(a, t, n * sizeof(int));
-
+void print(int a[], int n) {
+	for (int i = 0; i < n; i++)
+		std::cout << a[i] << " ";
+	std::cout << std::endl;
 }
 
+void merge(int a[], int n, int gap) {
+	for (int j = gap; j < n; j += gap) {
+		int key = a[j];
+		int i = j - gap;
+		while (i >= 0 && a[i] > key) {
+			a[i + gap] = a[i];
+			i -= gap;
+		}
+		a[i + gap] = key;
+	}
+}
 
-
-
-
-void ShellSort(int a[], int n)
-{
-	int gap;
-	int j;
-	int i;
-	int element;
-
-	for (gap = n / 2; gap > 0; gap /= 2)
+void shellsort(int a[], int n) {
+	for (int gap = n / 2; gap > 0; gap /= 2)
 		tbb::parallel_for(tbb::blocked_range<int>(0, gap),
 			[&](const tbb::blocked_range<int> &r) {
 				for (int i = r.begin(); i < r.end(); i++) {
-					merge(a + i, n - i, 0);
+					merge(a + i, n - i, gap);
 				}
 			});
-
-	{
-
-		for (i = gap; i < n; i++)
-		{
-
-			element = a[i];
-			for (j = i; j >= gap; j -= gap)
-			{
-
-				if (element < a[j - gap])
-					a[j] = a[j - gap];
-
-			}
-
-			a[j] = element;
-
-		}
-	}
 }
 
-void mergeSort(int a[], int t[], int n)
-{
-
-	if (n > 1) {
-
-		for (int i = 0; i < n / 2; i++) {
-			t[i] = a[i];
-		}
-
-	
-
-		for (int i = n / 2; i < n; i++) {
-			t[i] = a[i];
-		}
-		
-
-
-		mergeSort(a, t, n / 2);
-
-		ShellSort(a, n);
-
-
-	}
-}
-
-
-
-
-void print(int a[], int size)
-{
-
-	for (int i = 0; i < size; i++)
-		std::cout << a[i];
-	std::cout<<"";
-}
-
-
-
-
-
-int main(int argc, char *argv[])
-
-{
-	int threads=2;
-	
-	int  t[MAX_SIZE];
-	int n =2000;
+int main() {
+	int n = 2000;
 	int a[2000];
-	int i;
+
 	
+	const int threadNum = 2;
+
+	tbb::tick_count startTime;
+	double maintime;
+	double defineTime;
+	double parallelTime;
 
 	srand((time(NULL)));
 
 
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		a[i] = (rand() % 100000) + 1;
 
 	}
+
 	
-
-	tbb::tick_count start= tbb::tick_count::now();
-	tbb::tick_count stop = tbb::tick_count::now();
-
-	double maintimetbb, ParallelTime,ParallelTimeformerge;
-
-
-
-	std::cout << "enter element:";
+	std::cout << "enter elements :";
+	
 	std::cin >> n;
 	std::cout << "enter elements the one that you put earlier:";
+	
 	for (int i = 0; i < n; i++)
 	{
 		std::cin >> a[i];
 
 	}
 	std::cout << "array seq before sorting: ";
+	
+	
 	print(a, n);
-
-	std::cout << "\n maintimetbb " <<
-		(maintimetbb = ((tbb::tick_count::now()) - start).seconds());
-	start = tbb::tick_count::now();
-
 	
-
-
 	
-	//ShellSort(a, n);
-
-	std::cout << "\narray seq after sorting: ";
-	print(a, n);
-
+	startTime = tbb::tick_count::now();
 	
+	maintime = (tbb::tick_count::now() - startTime).seconds();
 
-	
+	tbb::task_scheduler_init init(1);
+	startTime = tbb::tick_count::now();
 
+	shellsort(a, n);
 
-	std::cout << "\nenter the size of the array";
-	std::cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		std::cin >> a[i];
+	defineTime = (tbb::tick_count::now() - startTime).seconds();
+	init.terminate();
 
+	init.initialize(threadNum);
+	startTime = tbb::tick_count::now();
+
+	shellsort(a, n);
+	parallelTime = (tbb::tick_count::now() - startTime).seconds();
+
+	init.terminate();
+
+	if (n < 100) {
+		std::cout << "Sorted array: ";
+		print(a, n);
 	}
+	shellsort(a, n);
 	
+		std::cout << "\nParallel shell sort " << std::endl;
 
+		std::cout << "maintimestarting: " << maintime << std::endl;
 
-	std::cout << "\narray seq before sorting: ";
+		std::cout << "\ndefine time:  " << defineTime << std::endl;
 
-	print(a, n);
+			std::cout << "\nparallel time: " << parallelTime << std::endl;
 
-	
-	for (int i = 0; i < n; i++) {
-		std::cout << "\nenter the element of shell:" << i;
-		std::cin >> a[i];
-	}
-	print(a, n);
-
-	std::cout << "\nSorted Array Elements with shell:";
-	for (int i = 0; i < n; i++) {
-		std::cout << a[i] << "\t";
-	}
+			std::cout << "\nparallel efficencty= ";
+		std::cout << defineTime / parallelTime << std::endl;
 
 	
-	
-	//mergeSort(a, t + 1, n);
-	ShellSort(a, n);
-	mergeSort(a, 0, n - 1);
-
-	parallel_sort(a, a + n);
-	std::cout << "\nParallelTime: ";
-	(ParallelTime = ((tbb::tick_count::now()) - stop).seconds());
-	stop = tbb::tick_count::now();
-
-
-	std::cout << (maintimetbb / ParallelTime ) << "\nmaintimetbb: ";
-	
-	ShellSort(a, n);
-	print(a, n);
-
-
-
-
+	return 0;
 }
-
-
-
-
 
 
 
