d321fd45d27babb096059c6cd8dd3671bdffb908
utshomomen <mohibulmomenutsho@yahoo.com>
2020-06-30 14:02:41 +0300

    thread2

--- 1708-2/stdshellthread.cpp
+++ 1708-2/stdshellthread.cpp
@@ -1,97 +1,204 @@
+// shellsortsequence.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//COPYRIGHT UTSHO_MOMEN
 #include "pch.h"
+#include <cstring>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
 #include <iostream>
 #include "malloc.h"
-#include "omp.h"
 #include <ctime>
 #include <cstdlib>
 #include <thread>
 #include <csignal>
 
-class ShellSort {
-	int a[1000], n;
+using namespace std;
 
-
-public:
-	
-
-    void merge();
-	void print();
-	void sortmain();
-};
+#define MAX_SIZE 1000
 
 
 
-void ShellSort::merge()
+void ShellSort(int a[], int n)
 {
-			std::cout << "type element: ";
-			std::cin >> n;
-			for (int i = 0; i < n; i++)
-				std::cin >> a[i];
-
-			if (n > 1) {
-				for (int i = 0; i < n / 2; i++) {
-					a[i] = a[i];
-				}
-
-				
-			}
-
-
-
-}
-
-
-
-void ShellSort::print() {
-	std::cout << "\n_print data_\n";
-	for (int i = 0; i < n; i++)
-		std::cout << a[i];
-	std::cout << "  ";
-}
-
-
-//t(temp) == element
-void ShellSort::sortmain() {
-	int i;
-	int j;
-	int element;
 	int gap;
+	int j;
+	int i;
+	int element;
 
-	for (gap = n / 2; gap > 0; gap /= 2) {
-		for (i = gap; i < n; i++) {
+	for (gap = n / 2; gap > 0; gap /= 2)
+	{
+
+		for (i = gap; i < n; i++)
+		{
+
 			element = a[i];
-			for ( j = i; j >= gap; j -= gap) {
+			for (j = i; j >= gap; j -= gap)
+			{
+
 				if (element < a[j - gap])
 					a[j] = a[j - gap];
-				else
-					break;
+
 			}
+
 			a[j] = element;
+
 		}
 	}
 }
 
-int main() {
 
-	std::cout << "\t0___0\t" ;
-	ShellSort t;
-	t.merge();
-	t.print();
-	t.sortmain();
-	
-	ShellSort s;
 
-	std::thread t1(&ShellSort::merge, &s);
-	std::thread t2(&ShellSort::sortmain, &s);
-	std::thread t3(&ShellSort::print, &s);
+void merge(int a[], int n, int t[]) {
+	int i = 0;
+	int j = n / 2;
+	int ti = 0;
 
-	t1.join();
-	t2.join();
-	t3.join();
+	while (i < n / 2 && j < n) {
+		if (a[i] < a[j]) {
+			t[ti] = a[i];
+			ti++; i++;
+		}
+		else {
+			t[ti] = a[j];
+			ti++; j++;
+		}
+	}
+	while (i < n / 2)
+	{
+		t[ti] = a[i];
+		ti++; i++;
+	}
+	while (j < n)
+	{
 
-	system("pause");
+		t[ti] = a[j];
+		ti++; j++;
+	}
+	memcpy(a, t, n * sizeof(int));
 
-	return 0;
+}
+
+
+void print(int a[], int size)
+{
+
+	for (int i = 0; i < size; i++)
+		cout << a[i] << "";
+	cout << endl;
+}
+
+
+
+void mergeSort(int a[], int t[], int n)
+{
+
+	if (n > 1) {
+
+		for (int i = 0; i < n / 2; i++) {
+			t[i] = a[i];
+		}
+
+		mergeSort(a, t, n / 2);
+
+		for (int i = n / 2; i < n; i++) {
+			t[i] = a[i];
+		}
+
+		mergeSort(a, t, n / 2);
+
+
+
+		ShellSort(a, n);
+		merge(a, 0, n + t);
+
+	}
+}
+
+
+void ShellSort_parallel(int a[], int n, int part) {
+	int threads = n / part;
+	pthread_t thread[threads];
+	for (int i = 0; i < threads; i++) {
+		print(a + i * part, part);
+		ShellSort(a + i * part, part);
+		
+
+	}
+}
+
+
+int main()
+
+{
+	int threads = 2;
+
+	int  t[MAX_SIZE];
+	int n = 2000;
+	int a[2000];
+	int i;
+	srand((time(NULL)));
+
+	for (i = 0; i < n; i++)
+	{
+		a[i] = (rand() % 100000) + 1;
+
+	}
+
+	double startTime = clock();
+
+	cout << "enter element:" << endl;
+	cin >> n;
+	/*
+	cout << "enter elements the one that you put earlier:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	cout << "array seq before sorting: ";
+	print(a, n);
+	ShellSort(a, n);
+	cout << "array seq after sorting: ";
+	print(a, n);
+	*/
+
+	cout << "enter the size of the array" << endl;
+	cin >> n;
+
+	cout << "please enter the elements of the array" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "enter the element of shell:" << i << endl;
+		cin >> a[i];
+	}
+	print(a, n);
+	ShellSort(a, n);
+	mergeSort(a, 0, n - 1);
+
+	cout << "\tSorted Array Elements with shell" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << "\t";
+	}
+
+
+	ShellSort(a, n);
+
+	//threads
+
+	print(a, n);
+	int part = n / threads;
+	ShellSort_parallel(a,n, part);
+
+	print(a, n);
+
+
+
+
+	double endTime = clock();
+	int totalTime = endTime - startTime;
+
+	cout << "This is the time it took to run.\n" << endl;
+	cout << totalTime / threads << n << endl;
 
 
 }
