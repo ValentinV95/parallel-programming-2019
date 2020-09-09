23bd2b5d66c674ed3f04f93ef83ee050b4f22db4
setysen <setysen@mail.ru>
2020-09-09 18:48:41 +0300

    Update

--- 1708-2/emshanov_ai/mpi1_2020.cpp
+++ 1708-2/emshanov_ai/mpi1_2020.cpp
@@ -2,154 +2,8 @@
 #include <string>
 #include <algorithm>
 #include <ctime>
-#include <utility>
 #include <random>
 #include <iostream>
-
-bool checkSort(std::vector<int> arr) {
-  return std::is_sorted(std::begin(arr), std::end(arr));
-}
-
-void Radix(std::vector<int> *vec) {
-  int max = getMax(vec);
-  for (int exp = 1; max / exp > 0; exp *= 10) {
-    countSort(vec, exp);
-  }
-}
-
-void countSort(std::vector<int> *vec, int exp) {
-  int size = vec->size();
-  std::vector<int> output(size);
-  int i, count[10] = { 0 };
-  for (i = 0; i < size; i++) {
-    count[(vec->at(i) / exp) % 10]++;
-  }
-  for (i = 1; i < 10; i++) {
-    count[i] += count[i - 1];
-  }
-  for (i = size - 1; i >= 0; i--) {
-    output[count[(vec->at(i) / exp) % 10] - 1] = vec->at(i);
-    count[(vec->at(i) / exp) % 10]--;
-  }
-  for (i = 0; i < size; i++) {
-    vec->at(i) = output[i];
-  }
-}
-
-int getMax(std::vector<int> *vec) {
-  int size = vec->size();
-  int max = vec->at(0);
-  for (int i = 1; i < size; i++) {
-    if (vec->at(i) > max) {
-      max = vec->at(i);
-    }
-  }
-  return max;
-}
-
-std::vector<int> EvenOddBatch(std::vector<int> vec1, std::vector<int> vec2) {
-  int size1 = vec1.size();
-  int size2 = vec2.size();
-  int size = size1 + size2;
-  std::vector<int> res(size);
-  int i = 0, j = 0, k = 0;
-
-  while ((j < size1) && (k < size2)) {
-    res[i] = vec1[j];
-    res[i + 1] = vec2[k];
-    i += 2;
-    j++;
-    k++;
-  }
-
-  if ((k >= size2) && (j < size1)) {
-    for (int l = i; l < size; l++) {
-      res[l] = vec1[j];
-      j++;
-    }
-  }
-
-  for (int i = 0; i < size - 1; i++) {
-    if (res[i] > res[i + 1]) {
-      std::swap(res[i], res[i + 1]);
-    }
-  }
-
-  return res;
-}
-
-std::vector<int> evenBatch(std::vector<int> vec1, std::vector<int> vec2) {
-  int size1 = vec1.size();
-  int size2 = vec2.size();
-  int res_size = size1 / 2 + size2 / 2 + size1 % 2 + size2 % 2;
-  std::vector<int> res(res_size);
-  int i1 = 0;
-  int i2 = 0;
-  int i = 0;
-
-  while ((i1 < size1) && (i2 < size2)) {
-    if (vec1[i1] <= vec2[i2]) {
-      res[i] = vec1[i1];
-      i1 += 2;
-    }
-    else {
-      res[i] = vec2[i2];
-      i2 += 2;
-    }
-    i++;
-  }
-
-  if (i1 >= size1) {
-    for (int l = i2; l < size2; l += 2) {
-      res[i] = vec2[l];
-      i++;
-    }
-  }
-  else {
-    for (int l = i1; l < size1; l += 2) {
-      res[i] = vec1[l];
-      i++;
-    }
-  }
-  return res;
-}
-
-std::vector<int> oddBatch(std::vector<int> vec1, std::vector<int> vec2) {
-  int size1 = vec1.size();
-  int size2 = vec2.size();
-  int res_size = size1 / 2 + size2 / 2;
-  std::vector<int> res(res_size);
-  int i1 = 1;
-  int i2 = 1;
-  int i = 0;
-
-  while ((i1 < size1) && (i2 < size2)) {
-    if (vec1[i1] <= vec2[i2]) {
-      res[i] = vec1[i1];
-      i1 += 2;
-    }
-    else {
-      res[i] = vec2[i2];
-      i2 += 2;
-    }
-    i++;
-  }
-
-  if (i1 >= size1) {
-    for (int l = i2; l < size2; l += 2) {
-      res[i] = vec2[l];
-      i++;
-    }
-  }
-  else {
-    for (int l = i1; l < size1; l += 2) {
-      res[i] = vec1[l];
-      i++;
-    }
-  }
-
-  return res;
-}
 
 std::vector<int> GetRandVector(int size) {
   if (size < 1)
@@ -159,22 +13,122 @@ std::vector<int> GetRandVector(int size) {
   for (int i = 0; i < size; i++) {
     vec[i] = gen() % 100;
   }
-
   return vec;
 }
 
+void check(int& a,int& b) {
+  if (a > b) std::swap(a,b);
+}
+
+void CoutMass(std::vector<int>& mas) {
+  for (int i = 0; i < mas.size(); i++) {
+    std::cout << " " << mas[i] << " ";
+  }
+  std::cout << "\n";
+}
+
+
+void merger(std::vector<int>& mas, int first, int last) {
+  int half = (unsigned int)(first + ((last - first) / 2));
+  std::vector<int> Temp(mas.size());
+  int jleft = first; 
+  int  jright = half+1;
+  for (int i = first; i <= last; i++)
+  {
+
+    if ((jright > last) || (jleft < (half + 1)) && (mas[jleft] < mas[jright]) && (jleft < (first + half + 1))) {
+      Temp[i] = mas[jleft];
+      jleft++;
+    }
+    else if (jright <= (last)){
+      Temp[i] = mas[jright];
+      jright++;
+    }
+  }
+  for (unsigned int i = first; i <= last; i++)
+    mas[i] = Temp[i];
+}
+
+
+void unmerger(std::vector<int>& mas, int first, int last) {
+    int half= (unsigned int)(first + ((last - first) / 2));
+    int nach = first;
+    std::vector<int> Temp(mas.size());
+    int jleft = first;
+    int jright = half+1;
+    if(first%2==0)
+    for (int i = first; i <= last; i++)
+    {
+      if (i % 2 == 0) {
+        Temp[jleft] = mas[i];
+        jleft++;
+      }
+      else {
+        Temp[jright] = mas[i];
+        jright++;
+      }
+    }
+    else
+      for (int i = first; i <= last; i++)
+      {
+        if (i % 2 == 0) {
+          Temp[jright] = mas[i];
+          jright++;
+        }
+        else {
+          Temp[jleft] = mas[i];
+          jleft++;
+        }
+      }
+
+    for (unsigned int i = first; i <= last; i++)
+      mas[i] = Temp[i];
+}
+
+void BatcherSort(std::vector<int>& mas, int first, int last) {
+  if (first == last - 1) {
+    check(mas[first], mas[last]);//сравнение элементов
+    return;
+  }
+  if (last < first + 2) return;
+ 
+  unmerger(mas, first, last);//Разделяем массив
+ int half = (unsigned int)(first + ((last - first) / 2));
+  BatcherSort(mas, first, half);
+  BatcherSort(mas, half + 1, last); //Рекурсия вызов для каждой части
+  merger(mas, first, last); //Совмещаем части
+  for (int i = first; i <= last; i += 2) {
+    if ((i + 1) > last) break;
+    check(mas[i], mas[i + 1]);
+  };
+  int halfSize = (unsigned int)((last - first + 1) / 2 - 1);
+  for (int i = first; i + halfSize <= last; i++)
+    check(mas[i], mas[i + halfSize]);
+}
+
 int main() {
-  std::vector<int> part1 = GetRandVector(10);
-  std::vector<int> part2 = GetRandVector(10);
-  std::sort(part1.begin(), part1.end());
-  std::sort(part2.begin(), part2.end());
-  std::vector<int> even = evenBatch(part1, part2);
-  std::vector<int> odd = oddBatch(part1, part2);
-  std::vector<int> res = EvenOddBatch(even, odd);
-  std::vector<int>stl_mege(20);
-  std::merge(part1.begin(), part1.end(), part2.begin(), part2.end(), stl_mege.begin());
-  if (stl_mege == res)
-    std::cout << "Sorting will complete!";
-  else
-    std::cout << "Sorting will down!";
+  int Len;
+  std::cout << "Lenght: ";
+  std::cin >> Len;
+  std::cout << "\n";
+
+
+  std::vector<int> mas = GetRandVector(Len);
+  //std::cout << "Before: "; 
+  //CoutMass(mas);
+  std::vector<int> mas1 = mas;
+  BatcherSort(mas,0,Len-1);
+  std::sort(mas1.begin(), mas1.end());
+  //std::cout << "After: ";
+  //CoutMass(mas);
+  bool flag = 1;
+  for (int i = 0; i < Len - 1; i++) {
+    if (mas[i]!=mas1[i]) {
+      flag = 0;
+      std::cout << "NERABOTAYET";
+      break;
+    } 
+  }
+  if (flag == 1) std::cout << "RABOTAET\n\n";
+  system("pause");
 }
